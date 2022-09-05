a89d2ae05f18ef06c0f37624d62fc69158b2bc3f
Renan Cavichi <renancavichi@gmail.com>
2022-09-05 17:29:12 -0300

    add logotype and nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import './Header.css'
+import { FaReact as ReactLogo } from 'react-icons/fa'
+import { NavLink, Link } from 'react-router-dom'
+
 
 const Header = () => {
   return (
-
-    
     <header className='Header'>
       <div className='container'>
-        <div>Logo</div>
-        <div>Menu</div>
+        <Link to='/'>
+          <div className='logotype'>
+            <ReactLogo className='icon-logo'/>
+            <h1>React Tutorial</h1>
+          </div>
+        </Link>
+        <nav>
+          <ul>
+            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/contact'>Hooks</NavLink></li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
