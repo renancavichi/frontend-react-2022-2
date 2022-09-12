3367c4554e9ba37724a38b77fd380c7dd9091e83
Renan Cavichi <renancavichi@gmail.com>
2022-09-12 17:56:53 -0300

    sample of usestate and useeffect

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,27 @@
-import { Link } from 'react-router-dom'
-import Footer from "../components/Footer"
+import {useState, useEffect} from 'react'
 import Header from "../components/Header"
 import "./Contact.css"
 import MainContainer from '../components/MainContainer'
+import Footer from '../components/Footer'
 
 const Contact = () => {
+  
+  const [count, setCount] = useState(0)
+  const [nameUser, setNameUser] = useState("Renan")
+  
+  useEffect(() => {
+    console.log(count)
+  },[count])
+
   return (
     <>
       <Header />
       <MainContainer>
-        <h1>Contact</h1>
-
-        <p>Aqui é o contact</p>
-        <Link to='/'>Home</Link>
+        <h1>Olá {nameUser}!</h1>
+        <h1>My number is: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Plus 1</button>
+        <br/><br/>
+        <button onClick={() => setNameUser("Marcos")}>Change name Marcos</button>
       </MainContainer>
       <Footer />
     </>
