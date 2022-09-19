87168b2361e7c7fb098055a693d42f609ba35c2f
Renan Cavichi <renancavichi@gmail.com>
2022-09-19 17:54:23 -0300

    add route user and not found

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import SignUp from './pages/SignUp';
+import User from './pages/User';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/user/:id' element={<User />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
