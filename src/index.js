3ea230e22ee2028cf4c66a8d28ccf9e103a760b6
Renan Cavichi <renancavichi@gmail.com>
2022-09-19 17:54:46 -0300

    remove semi comas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
 
 // Render react in root div from public/index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,4 +12,4 @@ root.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+)
