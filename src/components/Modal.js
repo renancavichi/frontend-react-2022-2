3e558c216818216c78f3270e6a066f2a55ad8fbe
Renan Cavichi <renancavichi@gmail.com>
2022-10-04 16:37:24 -0300

    fit indentation format

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,46 +1,46 @@
 import styled from 'styled-components'
-import {CgClose} from 'react-icons/cg'
+import { CgClose } from 'react-icons/cg'
 
-const Modal = ({showModal, children, setShowModal}) => {
- 
-    const handleClose = (event) => {
-        if(event.target.classList.contains("my-modal")){
-            setShowModal(false)
-        }
-    }
+const Modal = ({ showModal, children, setShowModal }) => {
 
-  return (
-    <OuterModal className={"my-modal"} open={showModal} onClick={(event)=>handleClose(event)}>
-        <InnerModal>
-            <CloseIcon onClick={() => setShowModal(false)} />
-            {children}
-        </InnerModal>
-    </OuterModal>
-  )
+	const handleClose = (event) => {
+		if (event.target.classList.contains("my-modal")) {
+			setShowModal(false)
+		}
+	}
+
+	return (
+		<OuterModal className={"my-modal"} open={showModal} onClick={(event) => handleClose(event)}>
+			<InnerModal>
+				<CloseIcon onClick={() => setShowModal(false)} />
+				{children}
+			</InnerModal>
+		</OuterModal>
+	)
 }
 
 const OuterModal = styled.div`
-position: fixed;
-display: ${props => props.open ? "flex" : "none"};
-top: 0;
-right: 0;
-bottom: 0;
-left: 0;
-background: #00000030;
-justify-content: center;
-align-items: center;
+	position: fixed;
+	display: ${props => props.open ? "flex" : "none"};
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background: #00000030;
+	justify-content: center;
+	align-items: center;
 `
 
 const InnerModal = styled.div`
-position: relative;
-width: 70%;
-max-width: 400px;
-height: auto;
-background: #FFF;
-border-radius: 15px;
-padding: 30px;
-& h1{
-margin: 0;
+	position: relative;
+	width: 70%;
+	max-width: 400px;
+	height: auto;
+	background: #FFF;
+	border-radius: 15px;
+	padding: 30px;
+	& h1{
+	margin: 0;
 }
 /* &:hover {
 background: #435345;
@@ -51,11 +51,11 @@ width: 95%;
 `
 
 const CloseIcon = styled(CgClose)`
-    position: absolute;
-    top: 15px;
-    right: 15px;
-    padding: 5px;
-    cursor: pointer;
+	position: absolute;
+	top: 15px;
+	right: 15px;
+	padding: 5px;
+	cursor: pointer;
 `
 
 export default Modal
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,54 +3,52 @@ import { useNavigate } from "react-router-dom"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import MainContainer from "../components/MainContainer"
-import { API_PATH  } from "../config"
+import { API_PATH } from "../config"
 import ButtonLoading from "../components/ButtonLoading"
 
-
 const SignUp = () => {
-    
-    const [isLoading, setIsLoading] = useState(false)
-    const navigate = useNavigate()
-    
-    const createUser = async (user) => {
-        const response = await fetch(`${API_PATH}user/sign-up`, {
-            method: 'POST',
-            body: JSON.stringify(user)
-        })
-        const result = await response.json()
-        if(result?.success){
-            navigate('/')
-        }
-        setIsLoading(false)
+
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const createUser = async (user) => {
+    const response = await fetch(`${API_PATH}user/sign-up`, {
+      method: 'POST',
+      body: JSON.stringify(user)
+    })
+    const result = await response.json()
+    if (result?.success) {
+      navigate('/')
     }
-  
-  
+    setIsLoading(false)
+  }
+
   const handleSubmit = (event) => {
     setIsLoading(true)
     event.preventDefault()
     const { name, email, pass, avatar } = event.target
     createUser({
-        name: name.value,
-        email: email.value,
-        pass: pass.value,
-        avatar: avatar.value
+      name: name.value,
+      email: email.value,
+      pass: pass.value,
+      avatar: avatar.value
     })
   }
 
   return (
     <>
-        <Header />
-        <MainContainer>
-            <h1>Sign Up</h1>
-            <form onSubmit={(event) => handleSubmit(event)}>
-                <p>Name: <input type="text" name="name"/></p>
-                <p>Email: <input type="text" name="email"/></p>
-                <p>Pass: <input type="password" name="pass"/></p>
-                <p>Avatar: <input type="text" name="avatar"/></p>
-                <ButtonLoading type="submit" isLoading={isLoading}>Send</ButtonLoading>
-            </form>
-        </MainContainer>
-        <Footer />
+      <Header />
+      <MainContainer>
+        <h1>Sign Up</h1>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <p>Name: <input type="text" name="name" /></p>
+          <p>Email: <input type="text" name="email" /></p>
+          <p>Pass: <input type="password" name="pass" /></p>
+          <p>Avatar: <input type="text" name="avatar" /></p>
+          <ButtonLoading type="submit" isLoading={isLoading}>Send</ButtonLoading>
+        </form>
+      </MainContainer>
+      <Footer />
     </>
   )
 }
