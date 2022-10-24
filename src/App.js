c82f58b16a77871df9802940e75a475c8c627b9a
Renan Cavichi <renancavichi@gmail.com>
2022-10-24 17:51:18 -0300

    add initialization and admin routes

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ import User from './pages/User';
 import NotFound from './pages/NotFound';
 import AuthProvider from './Providers/AuthProvider';
 import Login from './pages/Login';
+import HomeAdmin from './pages/HomeAdmin';
+import Layout from './pages/Layout';
+import LayoutAdmin from './pages/LayoutAdmin';
+import Initialization from './components/Initialization';
 
 const App = () => {
   return (
     <AuthProvider>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/user/:id' element={<User />} />
-        <Route path='*' element={<NotFound />} status={404} />
-      </Routes>
+      <Initialization>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='login' element={<Login />} />
+            <Route path='sign-up' element={<SignUp />} />
+            <Route path='user/:id' element={<User />} />
+          </Route>
+
+          <Route path='/admin' element={<LayoutAdmin />}>
+            <Route index element={<HomeAdmin />}/>
+          </Route>
+
+          <Route path='*' element={<NotFound />} status={404} />
+        </Routes>
+      </Initialization>
     </AuthProvider>
-   
   );
 }
 
